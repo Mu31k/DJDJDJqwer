6f9e7b72a58667083b839e9644c0fcd2b4d91e98
Muzikir <muzikir1217@yandex.ru>
2021-12-29 20:17:23 +0500

    add file

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -4,16 +4,12 @@ from django.forms import ModelForm, TextInput, DateTimeInput, Textarea
 class ArticlesForm(ModelForm):
     class Meta:
         model = Articles
-        fields = ['title', 'anons', 'full_text', 'date']
+        fields = ['title', 'full_text', 'date']
 
         widgets = {
             'title': TextInput(attrs={
                 'class': 'from-control',
                 'placeholder': 'Название статьи'
-            }),
-            'anons': TextInput(attrs={
-                'class': 'from-control',
-                'placeholder': 'Анонс статьи'
             }),
             'date': DateTimeInput(attrs={
                 'class': 'from-control',
--- mysite/news/models.py
+++ mysite/news/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Articles(models.Model):
     title = models.CharField('Название', max_length=100)
-    anons = models.CharField('Анонс', max_length=250)
+    # anons = models.CharField('Анонс', max_length=250)
     # img = models.ImageField(upload_to='article', height_field=100, width_field=100)
     full_text = models.TextField('Статья')
     date = models.DateTimeField('Дата публикации')
